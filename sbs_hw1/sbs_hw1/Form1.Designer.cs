e1c46123009a679a94527a761988393f02069de8
Nikolay <kaksusss@gmail.com>
2017-05-08 21:04:04 +0300

    X 0 version 1

--- sbs_hw1/sbs_hw1/Form1.Designer.cs
+++ sbs_hw1/sbs_hw1/Form1.Designer.cs
@@ -28,12 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.radioX = new System.Windows.Forms.RadioButton();
+            this.radio0 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(44, 44);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(62, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 44);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(112, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 44);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(112, 62);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(44, 44);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(62, 62);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(44, 44);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(12, 62);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(44, 44);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(112, 112);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(44, 44);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(62, 112);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(44, 44);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button9
+            // 
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(12, 112);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(44, 44);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.buttons_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(197, 23);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "start";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(112, 198);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 13);
+            this.result.TabIndex = 10;
+            // 
+            // radioX
+            // 
+            this.radioX.AutoSize = true;
+            this.radioX.Location = new System.Drawing.Point(229, 62);
+            this.radioX.Name = "radioX";
+            this.radioX.Size = new System.Drawing.Size(32, 17);
+            this.radioX.TabIndex = 11;
+            this.radioX.TabStop = true;
+            this.radioX.Text = "X";
+            this.radioX.UseVisualStyleBackColor = true;
+            // 
+            // radio0
+            // 
+            this.radio0.AutoSize = true;
+            this.radio0.Checked = true;
+            this.radio0.Location = new System.Drawing.Point(229, 86);
+            this.radio0.Name = "radio0";
+            this.radio0.Size = new System.Drawing.Size(31, 17);
+            this.radio0.TabIndex = 12;
+            this.radio0.TabStop = true;
+            this.radio0.Text = "0";
+            this.radio0.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(285, 166);
+            this.Controls.Add(this.radio0);
+            this.Controls.Add(this.radioX);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "X0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.RadioButton radioX;
+        private System.Windows.Forms.RadioButton radio0;
     }
 }
 
--- sbs_hw1/sbs_hw1/Form1.cs
+++ sbs_hw1/sbs_hw1/Form1.cs
@@ -12,9 +12,108 @@ namespace sbs_hw1
 {
     public partial class Form1 : Form
     {
+        string user_sing = "0";
+        string ai_sing = "X";
+        int step_count = 9; // число пустых клеток
+
+        Button[,] buttons;
         public Form1()
         {
             InitializeComponent();
+            buttons = new Button[,] { { button1, button2, button3 }, { button6, button5, button4 }, { button9, button8, button7 } };
         }
+
+        private void buttons_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Text != "")
+                return;
+            step_count--;
+            btn.Text = user_sing;
+            check();
+            ai_step();
+            check();
+        }
+
+        private void ai_step()
+        {
+           foreach(Button btn in buttons)
+            { 
+                if (btn.Text == "")
+                {
+                    btn.Text = ai_sing;
+                    break;
+                }
+            }
+            step_count--;
+        }
+
+        private void check()
+        {
+            string sign = "";
+            // вертикально
+            for(int i = 0; i < buttons.GetLength(0); i++)
+            {
+                if((buttons[i, 0].Text == buttons[i, 1].Text) && (buttons[i, 0].Text == buttons[i, 2].Text) && (buttons[i, 0].Text != ""))
+                    sign =  buttons[i, 0].Text;
+            }
+            // горизонтально
+            for (int i = 0; i < buttons.GetLength(1); i++)
+            {
+                if ((buttons[0, i].Text == buttons[1, i].Text) && (buttons[0, i].Text == buttons[2, i].Text) && (buttons[0, i].Text != ""))
+                    sign = buttons[0, i].Text;
+            }
+            // главная диагональ
+            if ((buttons[0, 0].Text == buttons[1, 1].Text) && (buttons[1, 1].Text == buttons[2, 2].Text) && (buttons[0, 0].Text != ""))
+                sign = buttons[0, 0].Text;
+            // побочная диагональ
+            if ((buttons[2, 0].Text == buttons[1, 1].Text) && (buttons[1, 1].Text == buttons[0, 2].Text) && (buttons[2, 0].Text != ""))
+                sign = buttons[2, 0].Text;
+            if (sign == user_sing)
+            {
+                result.Text = "Победа";
+                lock_board(false);
+            }
+            else if (sign == ai_sing)
+            {
+                result.Text = "Поражение";
+                lock_board(false);
+            }
+            else if (step_count == 0)
+            {
+                result.Text = "Ничья";
+                lock_board(false);
+            }
+        }
+
+        private void clear_board()
+        {
+            foreach (Button btn in buttons)
+                btn.Text = "";
+        }
+
+        private void lock_board(bool val)
+        {
+            foreach (Button btn in buttons)
+                btn.Enabled = val;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            clear_board();
+            step_count = 9;
+            lock_board(true);
+            result.Text = "";
+            if (radio0.Checked) {
+                user_sing = "0";
+                ai_sing = "X";
+            }
+            else
+            {
+                user_sing = "X";
+                ai_sing = "0";
+            }
+        }
+
     }
 }
